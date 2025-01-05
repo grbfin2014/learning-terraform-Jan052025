fbe4ef1a278b11d1c3c9fd4b3b1dd55742ec3062
grbfin2014 <grbfin2014@yahoo.com>
2025-01-05 12:38:14 -0800

    fixederrormessage-001

--- variables.tf
+++ variables.tf
@@ -4,14 +4,14 @@ variable "instance_type" {
 }
 
 #Define variables for secrets
-variable "username" {
- type = string
-}
-variable "password" {
- type = string
-}
+#variable "username" {
+# type = string
+#}
+#variable "password" {
+# type = string
+#}
 
 #Define variables for AWS access key, secret key, and region
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+#variable "aws_region" {}
