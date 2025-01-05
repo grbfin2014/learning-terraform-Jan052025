93eb36b93ac12582137620b7d4765accaba192ca
grbfin2014 <grbfin2014@yahoo.com>
2025-01-05 12:27:00 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+}
+
+#Define variables for secrets
+variable "username" {
+ type = string
+}
+variable "password" {
+ type = string
+}
+
+#Define variables for AWS access key, secret key, and region
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+#variable "aws_region" {}
