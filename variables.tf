273638df8431028eab9df92ff010c18f560f7ccc
grbfin2014 <grbfin2014@yahoo.com>
2025-01-05 12:29:29 -0800

    JAN052025-01

--- variables.tf
+++ variables.tf
@@ -12,6 +12,6 @@ variable "password" {
 }
 
 #Define variables for AWS access key, secret key, and region
-#variable "aws_access_key" {}
-#variable "aws_secret_key" {}
-#variable "aws_region" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
